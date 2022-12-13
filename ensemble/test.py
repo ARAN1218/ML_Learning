5942c57b69f32eda08f98d40963a452e3d7bd7ce
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-12-13 23:35:10 +0900

    Update test.py

--- ensemble/test.py
+++ ensemble/test.py
@@ -45,3 +45,66 @@ else:
     z = plf.predict(x)
     print(str(plf))
     print(z)
+
+    
+# Bagging テスト
+random.seed(1)
+import pandas as pd
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC, SVR
+from sklearn.gaussian_process import GaussianProcessClassifier, GaussianProcessRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+
+df = df_boston
+x = df[df.columns[:-1]]
+is_regression = True
+
+# ベンチマークとなるアルゴリズムと、アルゴリズムを実装したモデルの一覧
+classify_models = [ 
+    (LogisticRegression, {}),
+    (SVC, {'random_state':1}), 
+    (GaussianProcessClassifier, {'random_state':1}),
+    (KNeighborsClassifier, {}), 
+    (MLPClassifier, {'random_state':1}) 
+]
+
+regression_models = [
+    (LinearRegression, {}),
+    (SVR, {}),
+    (GaussianProcessRegressor, {'normalize_y':True, 'alpha':1, 'random_state':1}),
+    (KNeighborsRegressor, {}),
+    (MLPRegressor, {'hidden_layer_sizes':(5), 'solver':'lbfgs', 'random_state':1})
+]
+
+if not is_regression:
+    print('分類')
+    y = df[df.columns[-1]]
+    plf = Bagging(models=classify_models, ratio=1.0, is_regression=is_regression)
+    plf.fit(x, y)
+    print(str(plf))
+    z = plf.predict(x)
+    print(z)
+    
+    from sklearn.metrics import classification_report, f1_score, accuracy_score
+    print("accuracy:", accuracy_score(y, z))
+    print("f1_score:", f1_score(y, z, average='weighted'))
+else:
+    print('回帰')
+    y = df[df.columns[-1]]
+    plf = Bagging(models=regression_models, ratio=1.0, is_regression=is_regression)
+    plf.fit(x, y)
+    print(str(plf))
+    z = plf.predict(x)
+    print(z)
+
+    from sklearn.metrics import r2_score, explained_variance_score, mean_absolute_error, mean_squared_error
+    print('Train Score:')
+    rp = r2_score(y, z)
+    print('R2 Score: %f'%rp)
+    rp = explained_variance_score(y, z)
+    print('Explained Variance Score: %f'%rp)
+    rp = mean_absolute_error(y, z)
+    print('Mean Absolute Error: %f'%rp)
+    rp = mean_squared_error(y, z)
+    print('Mean Squared Error: %f'%rp)
